4204d47503d0e9051d015aaa6d3e2983b233af53
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-06-22 00:03:43 +0900

    [new feature] set world box from real to sim in argument of launch file

--- scripts/policy_navigator.py
+++ scripts/policy_navigator.py
@@ -87,7 +87,11 @@ class AgilePilotNode:
         self.state = None
         self.poll_y = np.array([0,1,0],dtype="float32")
         self.goal_lin_vel = np.array([5,0,0],dtype="float32")
-        self.world_box = np.array([-0.3, 70 ,-1.5, 1.5, 0.2, 2.0],dtype="float32")
+        self.real_area = rospy.get_param("~real_area")
+        if self.real_area:
+            self.world_box = np.array([-0.3, 4.2 ,-1.5, 1.5, 0.2, 2.0],dtype="float32")
+        else:
+            self.world_box = np.array([-0.3, 60 ,-1.5, 1.5, 0.2, 2.0],dtype="float32")
         # should change when changing position
         x = rospy.get_param("~shift_x")
         y = rospy.get_param("~shift_y")
