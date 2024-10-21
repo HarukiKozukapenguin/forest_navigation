2c15649b26d7e2e3879074b2a2a569592e417492
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-10-21 20:50:01 +0900

    [WIP] set obstacle avoidnace system in 6 phase

--- scripts/acc_check_exec.py
+++ scripts/acc_check_exec.py
@@ -28,9 +28,13 @@ class AgileQuadState:
                                quad_state.twist.twist.angular.z], dtype=np.float32)
 
 class GeoCondition:
-    pos_acc = 0
-    buffer_place = 1
-    neg_acc = 2
+    speed_up = 0
+    test = 1
+    move_to_test = 2
+
+class PosNeg:
+    pos = 0
+    neg = 1
 
 class AccCheck:
     def __init__(self) -> None:
@@ -51,16 +55,19 @@ class AccCheck:
     
     def set_variable(self):
         self.exec_max_gain = 0.4
-        self.check_distance = 0.5
+        self.speed_up_distance = 0.5
         self.stop_distance = 0.5
         self.x_range = 2.0
         self.vel_threshold = 0.6
+        self.vel_stopped = 0.1
+        self.pos_start = 0.1
     
     def initialize_variable(self):
         self.publish_commands = False
         self.pos_nav_mode = False
         self.command = FlightNav()
-        self.geo_condition = GeoCondition.pos_acc
+        self.geo_condition = GeoCondition.speed_up
+        self.pos_neg = PosNeg.pos
         self.change_to_pos = True
         self.state = None
 
@@ -85,123 +92,186 @@ class AccCheck:
     def state_callback(self, state_data):
         self.command.header.stamp = state_data.header.stamp
         self.state = AgileQuadState(state_data, self.translation_position)
-        if self.state.pos[0] < self.check_distance:
-            self.geo_condition = GeoCondition.pos_acc
-        elif self.state.pos[0] < self.x_range - self.check_distance:
-            self.geo_condition = GeoCondition.buffer_place
+        if self.pos_neg == PosNeg.pos:
+            x_pos = self.state.pos[0]
+        if self.pos_neg == PosNeg.neg:
+            x_pos = -self.state.pos[0]
+        vel_x = self.state.vel[0]
+
+        if x_pos < self.speed_up_distance: 
+            self.geo_condition = GeoCondition.speed_up
+        elif x_pos < self.x_range - self.check_distance and self.vel_stopped < abs(vel_x):
+            self.geo_condition = GeoCondition.test
         else:
-            self.geo_condition = GeoCondition.neg_acc
+            self.geo_condition = GeoCondition.move_to_test
 
         if self.publish_commands:
             self.linvel_pub.publish(self.command)
 
-class PosAcc(smach.State):
+class PosSpeedUp(smach.State):
     def __init__(self, acc_check_node):
-        smach.State.__init__(self, outcomes=['neg_acc_prep','exit'])
+        smach.State.__init__(self, outcomes=['PosTest','exit'])
         self.counter = 0
         self.inverval_num = rospy.get_param("~inverval_num")
         self.acc_check_node = acc_check_node
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state PosAcc')
+        rospy.loginfo('Executing state PosSpeedup')
         while self.acc_check_node.state is None:
             rospy.sleep(0.01)
         if self.counter < self.inverval_num:
             self.counter += 1
             print("counter: ", self.counter)
-            while acc_check_node.geo_condition == GeoCondition.pos_acc:
-                self.act_set()
+            while acc_check_node.geo_condition == GeoCondition.speed_up:
+                self.vel_set()
                 rospy.sleep(0.01)
-            return 'neg_acc_prep'
+            return 'PosTest'
         else:
             self.finish()
             while True:
                 rospy.sleep(1)
             return 'exit'
 
+    def vel_set(self):
+        self.acc_check_node.command.target_pos_x = self.acc_check_node.state.pos[0]+self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_vel_x = self.acc_check_node.vel_threshold
+        self.acc_check_node.command.target_acc_x = 0.0
+
+    def finish(self):
+        self.acc_check_node.command.pos_xy_nav_mode = 2 #position mode
+        self.acc_check_node.command.target_pos_x = self.acc_check_node.speed_up_distance  + self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_vel_x = 0.0
+
+class PosTest(PosSpeedUp):
+    def __init__(self, acc_check_node):
+        smach.State.__init__(self, outcomes=['PosMoveToInit'])
+        self.acc_check_node = acc_check_node
+
+   def execute(self, userdata):
+        rospy.loginfo('Executing state PosTest')
+        while self.acc_check_node.state is None:
+            rospy.sleep(0.01)
+        while acc_check_node.geo_condition == GeoCondition.test:
+            self.act_set()
+            rospy.sleep(0.01)
+            rospy.loginfo("vel: %f", self.acc_check_node.state.vel[0])
+            rospy.loginfo("vel_threshold: %f", self.acc_check_node.vel_threshold)
+        if acc_check_node.geo_condition == GeoCondition.move_to_test:
+            return 'PosMoveToInit'
+
     def act_set(self):
         self.acc_check_node.command.target_pos_x = self.acc_check_node.state.pos[0]+self.acc_check_node.translation_position[0]
         self.acc_check_node.command.target_vel_x = self.acc_check_node.state.vel[0]
         action = np.array([self.acc_check_node.exec_max_gain, 0.0])
         self.acc_check_node.command.target_acc_x = action[0]
-    
-    def finish(self):
-        self.acc_check_node.command.pos_xy_nav_mode = 2 #position mode
-        self.acc_check_node.command.target_pos_x = self.acc_check_node.check_distance + self.acc_check_node.stop_distance + self.acc_check_node.translation_position[0]
-        self.acc_check_node.command.target_vel_x = 0.0
 
-class PosAccPrep(PosAcc):
+class PosMoveToInit(PosSpeedUp):
     def __init__(self, acc_check_node):
-        smach.State.__init__(self, outcomes=['pos_acc', 'neg_acc_prep'])
+        smach.State.__init__(self, outcomes=['NegSpeedUp'])
         self.acc_check_node = acc_check_node
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state PosAccPrep')
+        rospy.loginfo('Executing state PosMoveToInit')
         while self.acc_check_node.state is None:
             rospy.sleep(0.01)
-        while acc_check_node.geo_condition == GeoCondition.buffer_place and self.acc_check_node.state.vel[0] < acc_check_node.vel_threshold:
-            self.act_set()
+        while acc_check_node.geo_condition == GeoCondition.move_to_test and acc_check_node.pos_start < abs(self.acc_check_node.state.pos[0] - x_range):
+            self.pos_set()
             rospy.sleep(0.01)
             rospy.loginfo("vel: %f", self.acc_check_node.state.vel[0])
             rospy.loginfo("vel_threshold: %f", self.acc_check_node.vel_threshold)
             rospy.loginfo("self.acc_check_node.state.vel[0] < acc_check_node.vel_threshold: %d", self.acc_check_node.state.vel[0] < acc_check_node.vel_threshold)
-        if acc_check_node.geo_condition == GeoCondition.pos_acc:
-            return 'pos_acc'
-        else:
-            return 'neg_acc_prep'
+        self.acc_check_node.geo_condition == GeoCondition.speed_up:
+        self.acc_check_node.pos_neg = PosNeg.neg
+        return 'NegSpeedup'
 
-class NegAcc(smach.State):
+    def pos_set(self):
+        self.acc_check_node.command.target_pos_x = x_range + self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_vel_x = 0.0
+        self.acc_check_node.command.target_acc_x = 0.0
+
+
+class NegSpeedUp(PosSpeedUp):
     def __init__(self, acc_check_node):
-        smach.State.__init__(self, outcomes=['pos_acc_prep'])
+        smach.State.__init__(self, outcomes=['NegTest'])
         self.acc_check_node = acc_check_node
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state NegAcc')
+        rospy.loginfo('Executing state NegSpeedUp')
         while self.acc_check_node.state is None:
             rospy.sleep(0.01)
-        while acc_check_node.geo_condition == GeoCondition.neg_acc:
+        while acc_check_node.geo_condition == GeoCondition.speed_up:
+            self.vel_set()
+            rospy.sleep(0.01)
+        return 'NegTest'
+
+    def vel_set(self):
+        self.acc_check_node.command.target_pos_x = self.acc_check_node.state.pos[0]+self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_vel_x = -self.acc_check_node.vel_threshold
+        self.acc_check_node.command.target_vel_x = 0.0
+
+class NegTest(PosTest):
+    def __init__(self, acc_check_node):
+        smach.State.__init__(self, outcomes=['NegMoveToInit'])
+        self.acc_check_node = acc_check_node
+
+   def execute(self, userdata):
+        rospy.loginfo('Executing state NegTest')
+        while self.acc_check_node.state is None:
+            rospy.sleep(0.01)
+        while acc_check_node.geo_condition == GeoCondition.test:
             self.act_set()
             rospy.sleep(0.01)
-        return 'pos_acc_prep'
+            rospy.loginfo("vel: %f", self.acc_check_node.state.vel[0])
+            rospy.loginfo("vel_threshold: %f", self.acc_check_node.vel_threshold)
+        if acc_check_node.geo_condition == GeoCondition.move_to_test:
+            return 'NegMoveToInit'
 
     def act_set(self):
-        self.acc_check_node.command.target_pos_x = self.acc_check_node.state.pos[0] + self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_pos_x = self.acc_check_node.state.pos[0]+self.acc_check_node.translation_position[0]
         self.acc_check_node.command.target_vel_x = self.acc_check_node.state.vel[0]
         action = np.array([-self.acc_check_node.exec_max_gain, 0.0])
         self.acc_check_node.command.target_acc_x = action[0]
 
-class NegAccPrep(NegAcc):
+class NegMoveToInit(PosMoveToInit):
     def __init__(self, acc_check_node):
-        smach.State.__init__(self, outcomes=['neg_acc', 'pos_acc_prep'])
+        smach.State.__init__(self, outcomes=['PospeedUp'])
         self.acc_check_node = acc_check_node
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state NegAccPrep')
+        rospy.loginfo('Executing state PosMoveToInit')
         while self.acc_check_node.state is None:
             rospy.sleep(0.01)
-        while acc_check_node.geo_condition == GeoCondition.buffer_place and -self.acc_check_node.vel_threshold < self.acc_check_node.state.vel[0]:
-            self.act_set()
+        while acc_check_node.geo_condition == GeoCondition.move_to_test and acc_check_node.pos_start < abs(self.acc_check_node.state.pos[0]):
+            self.pos_set()
             rospy.sleep(0.01)
             rospy.loginfo("vel: %f", self.acc_check_node.state.vel[0])
-            rospy.loginfo("-vel_threshold: %f", -self.acc_check_node.vel_threshold)
-            rospy.loginfo("-self.acc_check_node.state.vel[0] < -acc_check_node.vel_threshold: %d", -self.acc_check_node.vel_threshold < self.acc_check_node.state.vel[0])
-        if acc_check_node.geo_condition == GeoCondition.neg_acc:
-            return 'neg_acc'
-        else:
-            return 'pos_acc_prep'
+            rospy.loginfo("vel_threshold: %f", self.acc_check_node.vel_threshold)
+            rospy.loginfo("self.acc_check_node.state.vel[0] < acc_check_node.vel_threshold: %d", self.acc_check_node.state.vel[0] < acc_check_node.vel_threshold)
+        self.acc_check_node.geo_condition == GeoCondition.speed_up:
+        self.acc_check_node.pos_neg = PosNeg.neg
+        return 'PosSpeedup'
+
+    def pos_set(self):
+        self.acc_check_node.command.target_pos_x = self.acc_check_node.translation_position[0]
+        self.acc_check_node.command.target_vel_x = 0.0
+        self.acc_check_node.command.target_acc_x = 0.0
 
 if __name__ == '__main__':
     acc_check_node = AccCheck()
     sm = smach.StateMachine(outcomes=['exit'])
     with sm:
-        smach.StateMachine.add('POSACC', PosAcc(acc_check_node),
-                               transitions={'neg_acc_prep':'NEGACCPREP'})
-        smach.StateMachine.add('NEGACCPREP', NegAccPrep(acc_check_node),
-                               transitions={'neg_acc':'NEGACC', 'pos_acc_prep':'POSACCPREP'})
-        smach.StateMachine.add('NEGACC', NegAcc(acc_check_node),
-                               transitions={'pos_acc_prep':'POSACCPREP'})
-        smach.StateMachine.add('POSACCPREP', PosAccPrep(acc_check_node),
-                               transitions={'pos_acc':'POSACC', 'neg_acc_prep':'NEGACCPREP'})
+        smach.StateMachine.add('POSSPEEDUP', PosSpeedUp(acc_check_node),
+                               transitions={'PosTest':'POSTEST'})
+        smach.StateMachine.add('POSTEST', PosTest(acc_check_node),
+                               transitions={'PosMoveToInit':'POSMOVETOINIT'})
+        smach.StateMachine.add('POSMOVETOINIT', PosMoveToInit(acc_check_node),
+                               transitions={'NegSpeedUp':'NEGSPEEDUP'})
+        smach.StateMachine.add('NEGSPEEDUP', NegSpeedUp(acc_check_node),
+                               transitions={'NegTest':'NEGTEST'})
+        smach.StateMachine.add('NEGTEST', NegTest(acc_check_node),
+                               transitions={'NegMoveToInit':'NEGMOVETOINIT'})
+        smach.StateMachine.add('NEGMOVETOINIT', NegMoveToInit(acc_check_node),
+                               transitions={'PosSpeedUp':'POSSPEEDUP'})
 
     sis = smach_ros.IntrospectionServer('acc_check_server', sm, '/ACC_CHECK')
     sis.start()
